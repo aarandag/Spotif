140c2744c81dffcd99ced3d44c4c590f24036c19
Alberto Aranda <alb.aranda@gmail.com>
2017-11-15 19:09:48 +0100

    Iteration 1

---
+++ server/manage_songs_and_albums/src/main/java/Persistence/UserDao.java
@@ -0,0 +1,22 @@
+package Persistence;
+
+import Persistence2.*;
+
+public class UserDao extends GeneralDao {
+
+	public UserDao() {
+		// TODO - implement UserDao.UserDao
+		throw new UnsupportedOperationException();
+	}
+
+	public void findByName() {
+		// TODO - implement UserDao.findByName
+		throw new UnsupportedOperationException();
+	}
+
+	public void findByID() {
+		// TODO - implement UserDao.findByID
+		throw new UnsupportedOperationException();
+	}
+
+}
