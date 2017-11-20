566e957e56ca3ea69e12546c663cdb90ae794a08
Alberto Aranda <alb.aranda@gmail.com>
2017-11-20 17:00:14 +0100

    Iteration 4

---
+++ server/manage_user_account/src/main/java/Persistence/PurchaseDao.java
@@ -0,0 +1,21 @@
+package Persistence;
+
+import Domain.*;
+
+public class PurchaseDao extends GeneralDao {
+
+	public PurchaseDao() {
+		// TODO - implement PurchaseDao.PurchaseDao
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param buyer
+	 */
+	public Purchase findByUser(User buyer) {
+		// TODO - implement PurchaseDao.findByUser
+		throw new UnsupportedOperationException();
+	}
+
+}
