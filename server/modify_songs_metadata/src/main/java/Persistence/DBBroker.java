10508f091f3287676a0efbbb95cfc2e756751db9
Javier Abengózar <32295191+SrBuwe@users.noreply.github.com>
2017-12-21 17:20:31 +0100

    Update DBBroker.java

--- server/modify_songs_metadata/src/main/java/Persistence/DBBroker.java
+++ server/modify_songs_metadata/src/main/java/Persistence/DBBroker.java
@@ -15,7 +15,7 @@ public class DBBroker {
 	 * 
 	 * @param query
 	 */
-	public ResultSet read(string query) {
+	public ResultSet read(String query) {
 		// TODO - implement DBBroker.read
 		throw new UnsupportedOperationException();
 	}
@@ -29,7 +29,7 @@ public class DBBroker {
 	 * 
 	 * @param query
 	 */
-	public int insert(string query) {
+	public int insert(String query) {
 		// TODO - implement DBBroker.insert
 		throw new UnsupportedOperationException();
 	}
@@ -38,7 +38,7 @@ public class DBBroker {
 	 * 
 	 * @param query
 	 */
-	public int delete(string query) {
+	public int delete(String query) {
 		// TODO - implement DBBroker.delete
 		throw new UnsupportedOperationException();
 	}
